ec7c5c67f830cf8d275f75eee188cd6847478845
Yonas Fesehatsion <yone920@gmail.com>
2019-04-15 17:54:56 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+name_list = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+
+def badge_maker(array)
+total_name = array.size 
+printed_name = 0
+
+  while printed_name < total_name 
+    puts "Hello, my name is #{array[printed_name]}."
+    printed_name += 1 
+  end
+end
