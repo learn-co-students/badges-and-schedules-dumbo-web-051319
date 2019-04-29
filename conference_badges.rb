ef5698c15de3e86653819b68e1553c14fea3bb0e
Abdul <awsayed@alaska.edu>
2019-04-29 22:31:57 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,4 +20,11 @@ end
 def printer(attendees)
 	batch_badge_creator(attendees).each { |message| puts message }
 	assign_rooms(attendees).each { |message| puts message }
+<<<<<<< HEAD
 end
+=======
+end
+
+# attendees = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
+printer(attendees)
+>>>>>>> 7216f139bb1eca1c68e26a427ac7eee32c389111
